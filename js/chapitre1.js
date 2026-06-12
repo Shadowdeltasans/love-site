6d45f532a504a5fb87f934c9b849c8dac0e6642c
Shadowdeltasans <arthurgraber68@gmail.com>
2026-06-12 02:53:55 +0200

    début du js et du html

---
+++ js/chapitre1.js
@@ -0,0 +1,17 @@
+const phrase = ["I","LOVE","YOU"] ;
+let phraseIndex = 0
+let letterIndex = 0
+
+
+function typeWriter() {
+    // si letterIndex < longueur de la phrase actuelle
+        // ajouter la lettre au HTML
+        // letterIndex + 1
+        // setTimeout(typeWriter, 100)
+    // sinon (phrase terminée)
+        // letterIndex = 0
+        // phraseIndex + 1
+        // setTimeout(typeWriter, 1000) ← pause plus longue entre phrases
+}
+
+typeWriter() // démarrer
